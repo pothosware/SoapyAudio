2fd804f3a807f0de6fc1495a235e3216a76ceee5
Charles J. Cliffe <cj@cubicproductions.com>
2015-12-27 19:29:36 -0500

    First pass at RtAudio RX input

---
+++ Settings.cpp
@@ -0,0 +1,345 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2015 Charles J. Cliffe
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "SoapyAudio.hpp"
+
+SoapyAudio::SoapyAudio(const SoapySDR::Kwargs &args)
+{
+    deviceId = -1;
+
+    asFormat = AUDIO_FORMAT_FLOAT32;
+
+    sampleRate = 48000;
+    centerFrequency = 0;
+
+    bufferLength = DEFAULT_BUFFER_LENGTH;
+    numBuffers = DEFAULT_NUM_BUFFERS;
+
+    agcMode = false;
+
+    bufferedElems = 0;
+    resetBuffer = false;
+
+    if (args.count("device_id") != 0)
+    {
+        try {
+            deviceId = std::stoi(args.at("device_id"));
+        } catch (const std::invalid_argument &) {
+        }
+        
+        int numDevices = dac.getDeviceCount();
+        
+        if (deviceId < 0 || deviceId >= numDevices)
+        {
+            throw std::runtime_error(
+                    "device_id out of range [0 .. " + std::to_string(numDevices) + "].");
+        }
+  
+        SoapySDR_logf(SOAPY_SDR_DEBUG, "Found Audio device using 'device_id' = %d", deviceId);
+    }
+    
+    if (deviceId == -1) {
+        throw std::runtime_error("device_id missing.");
+    }
+
+    RtAudio endac;
+    
+    devInfo = endac.getDeviceInfo(deviceId);
+
+    inputParameters.deviceId = deviceId;    
+    inputParameters.nChannels = 1;
+    inputParameters.firstChannel = 0;
+}
+
+SoapyAudio::~SoapyAudio(void)
+{
+    //cleanup device handles
+}
+
+/*******************************************************************
+ * Identification API
+ ******************************************************************/
+
+std::string SoapyAudio::getDriverKey(void) const
+{
+    return "Audio";
+}
+
+std::string SoapyAudio::getHardwareKey(void) const
+{
+    return "Audio";
+}
+
+SoapySDR::Kwargs SoapyAudio::getHardwareInfo(void) const
+{
+    //key/value pairs for any useful information
+    //this also gets printed in --probe
+    SoapySDR::Kwargs args;
+
+    args["origin"] = "https://github.com/pothosware/SoapyAudio";
+    args["device_id"] = std::to_string(deviceId);
+
+    return args;
+}
+
+/*******************************************************************
+ * Channels API
+ ******************************************************************/
+
+size_t SoapyAudio::getNumChannels(const int dir) const
+{
+    return (dir == SOAPY_SDR_RX) ? 1 : 0;
+}
+
+/*******************************************************************
+ * Antenna API
+ ******************************************************************/
+
+std::vector<std::string> SoapyAudio::listAntennas(const int direction, const size_t channel) const
+{
+    std::vector<std::string> antennas;
+    antennas.push_back("RX");
+    // antennas.push_back("TX");
+    return antennas;
+}
+
+void SoapyAudio::setAntenna(const int direction, const size_t channel, const std::string &name)
+{
+    // TODO
+}
+
+std::string SoapyAudio::getAntenna(const int direction, const size_t channel) const
+{
+    return "RX";
+    // return "TX";
+}
+
+/*******************************************************************
+ * Frontend corrections API
+ ******************************************************************/
+
+bool SoapyAudio::hasDCOffsetMode(const int direction, const size_t channel) const
+{
+    return false;
+}
+
+/*******************************************************************
+ * Gain API
+ ******************************************************************/
+
+std::vector<std::string> SoapyAudio::listGains(const int direction, const size_t channel) const
+{
+    //list available gain elements,
+    //the functions below have a "name" parameter
+    std::vector<std::string> results;
+
+    results.push_back("AUDIO");
+
+    return results;
+}
+
+bool SoapyAudio::hasGainMode(const int direction, const size_t channel) const
+{
+    return true;
+}
+
+void SoapyAudio::setGainMode(const int direction, const size_t channel, const bool automatic)
+{
+    agcMode = automatic;
+    SoapySDR_logf(SOAPY_SDR_DEBUG, "Setting Audio AGC: %s", automatic ? "Automatic" : "Manual");
+}
+
+bool SoapyAudio::getGainMode(const int direction, const size_t channel) const
+{
+    return agcMode;
+}
+
+void SoapyAudio::setGain(const int direction, const size_t channel, const double value)
+{
+    //set the overall gain by distributing it across available gain elements
+    //OR delete this function to use SoapySDR's default gain distribution algorithm...
+    SoapySDR::Device::setGain(direction, channel, value);
+}
+
+void SoapyAudio::setGain(const int direction, const size_t channel, const std::string &name, const double value)
+{
+    if (name == "AUDIO")
+    {
+        audioGain = value;
+        SoapySDR_logf(SOAPY_SDR_DEBUG, "Setting Audio Gain: %f", audioGain);
+    }
+}
+
+double SoapyAudio::getGain(const int direction, const size_t channel, const std::string &name) const
+{
+    if ((name.length() >= 2) && (name.substr(0, 2) == "AUDIO"))
+    {
+        return audioGain;
+    }
+
+    return 0;
+}
+
+SoapySDR::Range SoapyAudio::getGainRange(const int direction, const size_t channel, const std::string &name) const
+{
+    return SoapySDR::Range(0, 100);
+}
+
+/*******************************************************************
+ * Frequency API
+ ******************************************************************/
+
+void SoapyAudio::setFrequency(
+        const int direction,
+        const size_t channel,
+        const std::string &name,
+        const double frequency,
+        const SoapySDR::Kwargs &args)
+{
+    if (name == "RF")
+    {
+        centerFrequency = (uint32_t) frequency;
+        resetBuffer = true;
+        SoapySDR_logf(SOAPY_SDR_DEBUG, "Setting center freq: %d", centerFrequency);
+    }
+}
+
+double SoapyAudio::getFrequency(const int direction, const size_t channel, const std::string &name) const
+{
+    if (name == "RF")
+    {
+        return (double) centerFrequency;
+    }
+
+    return 0;
+}
+
+std::vector<std::string> SoapyAudio::listFrequencies(const int direction, const size_t channel) const
+{
+    std::vector<std::string> names;
+    names.push_back("RF");
+    return names;
+}
+
+SoapySDR::RangeList SoapyAudio::getFrequencyRange(
+        const int direction,
+        const size_t channel,
+        const std::string &name) const
+{
+    SoapySDR::RangeList results;
+    if (name == "RF")
+    {
+        results.push_back(SoapySDR::Range(0, 6000000000));
+    }
+    return results;
+}
+
+SoapySDR::ArgInfoList SoapyAudio::getFrequencyArgsInfo(const int direction, const size_t channel) const
+{
+    SoapySDR::ArgInfoList freqArgs;
+
+    // TODO: frequency arguments
+
+    return freqArgs;
+}
+
+/*******************************************************************
+ * Sample Rate API
+ ******************************************************************/
+
+void SoapyAudio::setSampleRate(const int direction, const size_t channel, const double rate)
+{
+    sampleRate = rate;
+    resetBuffer = true;
+    SoapySDR_logf(SOAPY_SDR_DEBUG, "Setting sample rate: %d", sampleRate);
+    // TODO
+}
+
+double SoapyAudio::getSampleRate(const int direction, const size_t channel) const
+{
+    return sampleRate;
+}
+
+std::vector<double> SoapyAudio::listSampleRates(const int direction, const size_t channel) const
+{
+    std::vector<double> results;
+
+    RtAudio endac;
+    RtAudio::DeviceInfo info = endac.getDeviceInfo(deviceId);
+
+    std::vector<unsigned int>::iterator srate;
+
+    for (srate = info.sampleRates.begin(); srate != info.sampleRates.end(); srate++) {
+        results.push_back(*srate);
+    }
+
+    return results;
+}
+
+void SoapyAudio::setBandwidth(const int direction, const size_t channel, const double bw)
+{
+    SoapySDR::Device::setBandwidth(direction, channel, bw);
+}
+
+double SoapyAudio::getBandwidth(const int direction, const size_t channel) const
+{
+    return SoapySDR::Device::getBandwidth(direction, channel);
+}
+
+std::vector<double> SoapyAudio::listBandwidths(const int direction, const size_t channel) const
+{
+    std::vector<double> results;
+
+    return results;
+}
+
+/*******************************************************************
+ * Settings API
+ ******************************************************************/
+
+SoapySDR::ArgInfoList SoapyAudio::getSettingInfo(void) const
+{
+    SoapySDR::ArgInfoList setArgs;
+
+    return setArgs;
+}
+
+void SoapyAudio::writeSetting(const std::string &key, const std::string &value)
+{
+    // if (key == "")
+    // {
+    // }
+}
+
+std::string SoapyAudio::readSetting(const std::string &key) const
+{
+    // if (key == "") {
+    //     return "";
+    // }
+
+    // SoapySDR_logf(SOAPY_SDR_WARNING, "Unknown setting '%s'", key.c_str());
+    return "";
+}
+
+
---
+++ Streaming.cpp
@@ -0,0 +1,359 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2015 Charles J. Cliffe
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "SoapyAudio.hpp"
+#include <SoapySDR/Logger.hpp>
+#include <algorithm> //min
+#include <climits> //SHRT_MAX
+#include <cstring> // memcpy
+
+
+std::vector<std::string> SoapyAudio::getStreamFormats(const int direction, const size_t channel) const {
+    std::vector<std::string> formats;
+
+    formats.push_back("CS8");
+    formats.push_back("CS16");
+    formats.push_back("CF32");
+
+    return formats;
+}
+
+std::string SoapyAudio::getNativeStreamFormat(const int direction, const size_t channel, double &fullScale) const {
+     fullScale = 65536;
+     return "CS16";
+}
+
+SoapySDR::ArgInfoList SoapyAudio::getStreamArgsInfo(const int direction, const size_t channel) const {
+    SoapySDR::ArgInfoList streamArgs;
+
+    // SoapySDR::ArgInfo bufflenArg;
+    // bufflenArg.key = "bufflen";
+    // bufflenArg.value = "16384";
+    // bufflenArg.name = "Buffer Size";
+    // bufflenArg.description = "Number of bytes per buffer, multiples of 512 only.";
+    // bufflenArg.units = "bytes";
+    // bufflenArg.type = SoapySDR::ArgInfo::INT;
+    //
+    // streamArgs.push_back(bufflenArg);
+
+    return streamArgs;
+}
+
+/*******************************************************************
+ * Async thread work
+ ******************************************************************/
+
+
+static int _rx_callback(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames, double streamTime, RtAudioStreamStatus status,
+        void *ctx)
+{
+    //printf("_rx_callback\n");
+    SoapyAudio *self = (SoapyAudio *)ctx;
+    return self->rx_callback(inputBuffer, nBufferFrames, streamTime, status);
+}
+
+void SoapyAudio::rx_async_operation(void)
+{
+    //printf("rx_async_operation\n");
+    try {
+#ifndef _MSC_VER
+        opts.priority = sched_get_priority_max(SCHED_FIFO);
+#endif
+        //    opts.flags = RTAUDIO_MINIMIZE_LATENCY;
+        opts.flags = RTAUDIO_SCHEDULE_REALTIME;
+
+        dac.openStream(NULL, &inputParameters, RTAUDIO_FLOAT32, sampleRate, &bufferLength, &_rx_callback, (void *) this, &opts);
+        dac.startStream();
+    } catch (RtAudioError& e) {
+        throw std::runtime_error("RtAudio init error '" + e.getMessage());
+    }
+    //printf("rx_async_operation done!\n");
+}
+
+int SoapyAudio::rx_callback(void *inputBuffer, unsigned int nBufferFrames, double streamTime, RtAudioStreamStatus status)
+{
+    std::unique_lock<std::mutex> lock(_buf_mutex);
+
+    //printf("_rx_callback %d _buf_head=%d, numBuffers=%d\n", len, _buf_head, _buf_tail);
+
+    //overflow condition: the caller is not reading fast enough
+    if (_buf_count == numBuffers)
+    {
+        _overflowEvent = true;
+        return 0;
+    }
+
+    //copy into the buffer queue
+    auto &buff = _buffs[_buf_tail];
+    buff.resize(nBufferFrames);
+    std::memcpy(buff.data(), inputBuffer, nBufferFrames * sizeof(float));
+
+    //increment the tail pointer
+    _buf_tail = (_buf_tail + 1) % numBuffers;
+    _buf_count++;
+
+    //notify readStream()
+    _buf_cond.notify_one();
+    
+    return 0;
+}
+
+/*******************************************************************
+ * Stream API
+ ******************************************************************/
+
+SoapySDR::Stream *SoapyAudio::setupStream(
+        const int direction,
+        const std::string &format,
+        const std::vector<size_t> &channels,
+        const SoapySDR::Kwargs &args)
+{
+    //check the channel configuration
+    if (channels.size() > 1 or (channels.size() > 0 and channels.at(0) != 0))
+    {
+        throw std::runtime_error("setupStream invalid channel selection");
+    }
+
+    //check the format
+    if (format == "CF32")
+    {
+        SoapySDR_log(SOAPY_SDR_INFO, "Using format CF32.");
+        asFormat = AUDIO_FORMAT_FLOAT32;
+    }
+    else if (format == "CS16")
+    {
+        SoapySDR_log(SOAPY_SDR_INFO, "Using format CS16.");
+        asFormat = AUDIO_FORMAT_INT16;
+    }
+    else if (format == "CS8") {
+        SoapySDR_log(SOAPY_SDR_INFO, "Using format CS8.");
+        asFormat = AUDIO_FORMAT_INT8;
+    }
+    else
+    {
+        throw std::runtime_error(
+                "setupStream invalid format '" + format
+                        + "' -- Only CS8, CS16 and CF32 are supported by SoapyAudio module.");
+    }
+
+    // if (args.count("buflen") != 0)
+    // {
+    //     try
+    //     {
+    //         int bufferLength_in = std::stoi(args.at("buflen"));
+    //         if (bufferLength_in > 0)
+    //         {
+    //             bufferLength = bufferLength_in;
+    //         }
+    //     }
+    //     catch (const std::invalid_argument &){}
+    // }
+    // SoapySDR_logf(SOAPY_SDR_DEBUG, "Using buffer length %d", bufferLength);
+
+    //clear async fifo counts
+    _buf_tail = 0;
+    _buf_count = 0;
+    _buf_head = 0;
+
+    //allocate buffers
+    _buffs.resize(numBuffers);
+    for (auto &buff : _buffs) buff.reserve(bufferLength);
+    for (auto &buff : _buffs) buff.resize(bufferLength);
+
+    return (SoapySDR::Stream *) this;
+}
+
+void SoapyAudio::closeStream(SoapySDR::Stream *stream)
+{
+    _buffs.clear();
+}
+
+size_t SoapyAudio::getStreamMTU(SoapySDR::Stream *stream) const
+{
+    return bufferLength / BYTES_PER_SAMPLE;
+}
+
+int SoapyAudio::activateStream(
+        SoapySDR::Stream *stream,
+        const int flags,
+        const long long timeNs,
+        const size_t numElems)
+{
+    if (flags != 0) return SOAPY_SDR_NOT_SUPPORTED;
+    resetBuffer = true;
+    bufferedElems = 0;
+
+    //start the async thread
+    if (!_rx_async_thread.joinable()) {
+        _rx_async_thread = std::thread(&SoapyAudio::rx_async_operation, this);
+    }
+
+    return 0;
+}
+
+int SoapyAudio::deactivateStream(SoapySDR::Stream *stream, const int flags, const long long timeNs)
+{
+    if (flags != 0) return SOAPY_SDR_NOT_SUPPORTED;
+    if (_rx_async_thread.joinable())
+    {
+        dac.abortStream();
+        _rx_async_thread.join();
+    }
+    return 0;
+}
+
+int SoapyAudio::readStream(
+        SoapySDR::Stream *stream,
+        void * const *buffs,
+        const size_t numElems,
+        int &flags,
+        long long &timeNs,
+        const long timeoutUs)
+{
+    //this is the user's buffer for channel 0
+    void *buff0 = buffs[0];
+
+    //are elements left in the buffer? if not, do a new read.
+    if (bufferedElems == 0)
+    {
+        int ret = this->acquireReadBuffer(stream, _currentHandle, (const void **)&_currentBuff, flags, timeNs, timeoutUs);
+        if (ret < 0) return ret;
+        bufferedElems = ret;
+    }
+
+    size_t returnedElems = std::min(bufferedElems, numElems);
+
+    //convert into user's buff0
+    if (asFormat == AUDIO_FORMAT_FLOAT32)
+    {
+        float *ftarget = (float *) buff0;
+        std::complex<float> tmp;
+        for (size_t i = 0; i < returnedElems; i++)
+        {
+            ftarget[i * 2] = _currentBuff[i];
+            ftarget[i * 2 + 1] = 0;
+        }
+    }
+    else if (asFormat == AUDIO_FORMAT_INT16)
+    {
+        int16_t *itarget = (int16_t *) buff0;
+        std::complex<int16_t> tmp;
+        for (size_t i = 0; i < returnedElems; i++)
+        {
+            itarget[i * 2] = int16_t(_currentBuff[i] * 32767.0);
+            itarget[i * 2 + 1] = 0;
+        }
+    }
+    else if (asFormat == AUDIO_FORMAT_INT8)
+    {
+        int8_t *itarget = (int8_t *) buff0;
+        for (size_t i = 0; i < returnedElems; i++)
+        {
+            itarget[i * 2] = int8_t(_currentBuff[i]*127.0);
+            itarget[i * 2 + 1] = 0;
+        }
+    }
+
+    //bump variables for next call into readStream
+    bufferedElems -= returnedElems;
+    _currentBuff += returnedElems*BYTES_PER_SAMPLE;
+
+    //return number of elements written to buff0
+    if (bufferedElems != 0) flags |= SOAPY_SDR_MORE_FRAGMENTS;
+    else this->releaseReadBuffer(stream, _currentHandle);
+    return returnedElems;
+}
+
+/*******************************************************************
+ * Direct buffer access API
+ ******************************************************************/
+
+size_t SoapyAudio::getNumDirectAccessBuffers(SoapySDR::Stream *stream)
+{
+    return _buffs.size();
+}
+
+int SoapyAudio::getDirectAccessBufferAddrs(SoapySDR::Stream *stream, const size_t handle, void **buffs)
+{
+    buffs[0] = (void *)_buffs[handle].data();
+    return 0;
+}
+
+int SoapyAudio::acquireReadBuffer(
+    SoapySDR::Stream *stream,
+    size_t &handle,
+    const void **buffs,
+    int &flags,
+    long long &timeNs,
+    const long timeoutUs)
+{
+    std::unique_lock <std::mutex> lock(_buf_mutex);
+
+    //reset is issued by various settings
+    //to drain old data out of the queue
+    if (resetBuffer)
+    {
+        //drain all buffers from the fifo
+        _buf_head = (_buf_head + _buf_count) % numBuffers;
+        _buf_count = 0;
+        resetBuffer = false;
+        _overflowEvent = false;
+    }
+
+    //handle overflow from the rx callback thread
+    if (_overflowEvent)
+    {
+        //drain the old buffers from the fifo
+        _buf_head = (_buf_head + _buf_count) % numBuffers;
+        _buf_count = 0;
+        _overflowEvent = false;
+        SoapySDR::log(SOAPY_SDR_SSI, "O");
+        return SOAPY_SDR_OVERFLOW;
+    }
+
+    //wait for a buffer to become available
+    while (_buf_count == 0)
+    {
+        _buf_cond.wait_for(lock, std::chrono::microseconds(timeoutUs));
+        if (_buf_count == 0) return SOAPY_SDR_TIMEOUT;
+    }
+
+    //extract handle and buffer
+    handle = _buf_head;
+    _buf_head = (_buf_head + 1) % numBuffers;
+    buffs[0] = (void *)_buffs[handle].data();
+    flags = 0;
+
+    //return number available
+    return _buffs[handle].size() / BYTES_PER_SAMPLE;
+}
+
+void SoapyAudio::releaseReadBuffer(
+    SoapySDR::Stream *stream,
+    const size_t handle)
+{
+    //TODO this wont handle out of order releases
+    std::unique_lock <std::mutex> lock(_buf_mutex);
+    _buf_count--;
+}
